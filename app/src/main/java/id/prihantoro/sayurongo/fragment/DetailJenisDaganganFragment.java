439c016a14c6b7a74d97f5e659b55888e72fbb31
Wahyu Prihantoro <wahyuprihantoro21@gmail.com>
2016-10-20 11:42:19 +0700

    add spinner

--- app/src/main/java/id/prihantoro/sayurongo/fragment/DetailJenisDaganganFragment.java
+++ app/src/main/java/id/prihantoro/sayurongo/fragment/DetailJenisDaganganFragment.java
@@ -1,12 +1,15 @@
 package id.prihantoro.sayurongo.fragment;
 
 import android.support.v4.app.Fragment;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
-import org.androidannotations.annotations.Extra;
 import org.androidannotations.annotations.FragmentArg;
 import org.androidannotations.annotations.ViewById;
 
@@ -23,15 +26,58 @@ public class DetailJenisDaganganFragment extends Fragment {
     @ViewById(R.id.jenis)
     TextView jenisTextView;
 
+    @ViewById
+    Spinner nama;
+
+    @ViewById
+    EditText harga;
+    @ViewById
+    EditText satuan;
+    @ViewById
+    EditText kuantitas;
+
     @AfterViews
     void init() {
-        if (jenis!=null){
+        if (jenis != null) {
             jenisTextView.setText(jenis);
         }
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.buah, android.R.layout.simple_spinner_item);
+        if (jenis.toLowerCase().contains("buah")) {
+            adapter = ArrayAdapter.createFromResource(getContext(),
+                    R.array.buah, android.R.layout.simple_spinner_item);
+        } else if (jenis.toLowerCase().contains("sayur")) {
+            adapter = ArrayAdapter.createFromResource(getContext(),
+                    R.array.sayur, android.R.layout.simple_spinner_item);
+        } else if (jenis.toLowerCase().contains("laut")) {
+            adapter = ArrayAdapter.createFromResource(getContext(),
+                    R.array.makanan_laut, android.R.layout.simple_spinner_item);
+        } else if (jenis.toLowerCase().contains("olahan")) {
+            adapter = ArrayAdapter.createFromResource(getContext(),
+                    R.array.makanan_olahan, android.R.layout.simple_spinner_item);
+        } else if (jenis.toLowerCase().contains("masak")) {
+            adapter = ArrayAdapter.createFromResource(getContext(),
+                    R.array.bumbu_masak, android.R.layout.simple_spinner_item);
+        } else if (jenis.toLowerCase().contains("daging")) {
+            adapter = ArrayAdapter.createFromResource(getContext(),
+                    R.array.daging, android.R.layout.simple_spinner_item);
+        }
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        nama.setAdapter(adapter);
     }
 
     @Click
     void simpan() {
+        if (harga.getText().toString().isEmpty() || satuan.getText().toString().isEmpty() || kuantitas.getText().toString().isEmpty()) {
+            Toast.makeText(getContext(), "isian tidak valid", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getContext(), "berhasil disimpan", Toast.LENGTH_SHORT).show();
+            getActivity().finish();
+        }
+    }
+
+    @Click
+    void linearLayout() {
 
     }
 
